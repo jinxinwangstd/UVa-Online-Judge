b78f3250f75a6765ce4a7fd2a2846ecc90e5977f
Jinxin Wang <jianhuqiuxia@gmail.com>
2019-04-06 23:00:54 -0700

    SCC problem

--- 11838.cpp
+++ 11838.cpp
@@ -12,35 +12,36 @@ typedef vector<ii> vii;
 const int UNVISITED = -1;
 const int maxn = 2000 + 5;
 int n, m, dfs_counter, num_scc;
-vi dfs_num;
-vi dfs_low;
-vi S;
-vi visited;
-vi g[maxn];
+vi dfs_num;		// the index of traversing in DFS
+vi dfs_low;		// the lowest index a node can reach
+vi S;			// stack for storing vertices
+vi visited;		// record of whether it is in the DFS spanning tree
+vi g[maxn];		// graph represented in adjacent list
 
+// Tarjan's algorithm to identify strongly connected components
 void tarjanSCC(int u)
 {
-	dfs_num[u] = dfs_low[u] = dfs_counter++;
+	dfs_num[u] = dfs_low[u] = dfs_counter++;	// set initial dfs index
 	S.push_back(u);
-	visited[u] = 1;
+	visited[u] = 1;								// it is in the dfs spanning tree
 	int num_adj = (int) g[u].size();
 	for (int i = 0; i != num_adj; ++i)
 	{
 		int v = g[u][i];
-		if (dfs_num[v] == UNVISITED)
+		if (dfs_num[v] == UNVISITED)	// have not been traversed
 			tarjanSCC(v);
-		if (visited[v])
+		if (visited[v])					// it is in the dfs spanning tree
 			dfs_low[u] = min(dfs_low[u], dfs_low[v]);
 	}
-	if (dfs_num[u] == dfs_low[u])
+	if (dfs_num[u] == dfs_low[u])	// root of a strongly connected component
 	{
 		num_scc++;
-		while (1)
+		while (1)					// pop out members of this SCC
 		{
 			int v = S.back();
 			S.pop_back();
-			visited[v] = 0;
-			if (u == v)
+			visited[v] = 0;			// remove it from the spanning tree
+			if (u == v)				// all members of this SCC (including root) have been poped out
 				break;
 		}
 	}
@@ -53,7 +54,7 @@ int main()
 #endif
 	while (scanf("%d %d", &n, &m) && n && m)
 	{
-		for (int i = 0; i != n; ++i)
+		for (int i = 0; i != n; ++i)	// clear the graph
 			g[i].clear();
 		for (int i = 0; i != m; ++i)
 		{
@@ -63,10 +64,12 @@ int main()
 			if (d == 2)
 				g[v - 1].push_back(u - 1);
 		}
+		// Initialize all global variables
 		dfs_num.assign(n, UNVISITED);
 		dfs_low.assign(n, 0);
 		visited.assign(n, 0);
 		dfs_counter = num_scc = 0;
+		// Find all strongly connected components
 		for (int i = 0; i != n; ++i)
 			if (dfs_num[i] == UNVISITED)
 				tarjanSCC(i);
--- 11838_report.md
+++ 11838_report.md
@@ -4,9 +4,15 @@ Given a directed graph, determine whether the graph is strongly connected, i.e. 
 
 ## Algorithm analysis
 
-
+To solve this problem, we need to identify all strongly connected components in the graph.
+We can use tarjan's algorithm to find all SCC in a graph. It is a tweak of DFS algorithm.
+When we traverse the graph using DFS, we maintain two vectors dfs_num and dfs_low. The dfs_num record the index of traversing through each node, so that the first node we traverse would have a corresponding 0 in dfs_num. The other vector dfs_low record the lowest node it can reach, in other words, it has a path to the node.
+For a strongly connected component, there is only one node whose corresponding dfs_num value is the same as the dfs_low value, which is also the first node to be traversed; other nodes must have their dfs_low value less than their dfs_num value because they can reach any other node in the strongly connected component.
+In this way, we can count the number of such node to find the number of SCC in the graph and identify all nodes in a strongly connected component.
 
 ## Knowledge base
 
 1. vector can be used as stack.
+   It has a method back() to access the top element of the stack, and a pop_back() method to pop out the top element in the stack.
 2. Be careful about the vertex index. Sometimes it is 1-based index.
+   If it is 1-based index, we need to tweak their indices when constructing the graph.
